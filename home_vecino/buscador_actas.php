0d6c0f0257cedeed7e9e4300fd3c5a9ab93f79be
Felipe Andrés Opazo Córdova <felipe.opazo1901@alumnos.ubiobio.cl>
2021-12-15 17:08:26 -0300

    version casi casi

--- home_vecino/buscador_actas.php
+++ home_vecino/buscador_actas.php
@@ -10,66 +10,102 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <title>Actas</title>
+  
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="../tailwind/tailwind.output.css">
+<link href="https://cdn.jsdelivr.net/npm/@tailwindcss/custom-forms@0.2.1/dist/custom-forms.css" rel="stylesheet" />
 
+      <script src="https://cdn.tailwindcss.com"></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+	<style>
+		body {
+			font-family: 'Inter', sans-serif;
+		}
+		.form-control {
+  display: block;
+  width: 100%;
+  height: 34px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555555;
+  background-color: #ffffff;
+  background-image: none;
+  border: 1px solid #cccccc;
+  border-radius: 4px;
+  -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+  -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+  -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+  transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+}
+	</style>
+	
 
 </head>
 
 <body class="color-oscuro ">
-    <div class=" ">
-        <nav class="navbar navbar-expand-sm navbar-dark bg p-3 color-claro">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse nav " id="mynavbar">
-                    <ul class="navbar-nav me-auto estilos-textos ul">
-                        <li class="nav-item">
-                            <a class="links" href="#">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="links" href="../listado_reuniones/index.php">Reuniones</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="links" href="../registrar_comentario/index.php">Comentar Reuniones</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="links" href="../listado_reuniones/index.php">Actas</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="">
-                    <a class="btn btn-danger mr-3" href="../login/backend/cerrar.php">Cerrar Sesión</a>
-                </div>
-            </div>
-        </nav>
+    <div class="">
+        	<style>
+		#menu-toggle:checked+#menu {
+			display: block;
+		}
+	</style>
+	<header style="background:#141A32" class="lg:px-16 px-6 flex flex-wrap lg:flex-row-reverse items-center lg:py-6 py-6">
+		<div class="flex-1 lg:flex-initial flex justify-between items-center">
+			<a class="bg-red-500 px-4 py-2 rounded-md text-white" href="../login/backend/cerrar.php"> Cerrar Sesión </a>
+		</div>
+
+		<label for="menu-toggle" class="pointer-cursor lg:hidden block">
+			<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+				<title>menu</title>
+				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+			</svg>
+		</label>
+		<input class="hidden" type="checkbox" id="menu-toggle" />
+
+		<div class="hidden lg:flex-1 lg:flex lg:items-center lg:w-auto w-full text-white" id="menu">
+			<nav>
+				<ul class="lg:flex items-center justify-between text-base pt-4 lg:pt-0">
+					<li>
+						<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="./index.php">Inicio</a>
+					</li>
+					<li>
+						<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="../reuniones/index.php">Agendar Reunion</a>
+					</li>
+					<li>
+						<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Registrar Acta</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
         <?php include_once("../conexion_bd/conexion.php") ?>
-        <div class=" color-oscuro tw-px-4 tw-max-w-4xl tw-mx-auto">
-            <h1 class="text-white text-center tw-text-4xl tw-font-semibold">Buscador de actas</h1>
+        <div class=" color-oscuro px-4 max-w-4xl mx-auto">
+            <h1 class="text-center text-white text-4xl font-semibold">Buscador de actas</h1>
             <p class="text-white">Acá estan los resultados de las actas. Si tienes los permisos necesarios podrás crear un acta en la esquina inferior a la derecha.</p>
-            <form class=" m-auto tw-mt-4" method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
-                <div class="tw-grid tw-grid-cols-12  tw-gap-x-4 tw-gap-y-2">
-                    <div class="tw-col-span-12 lg:tw-col-span-4 ">
-                        <label class="text-white font-semibold " for="tema">Tema: </label>
+            <form class="m-auto mt-4" method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+                <div class="grid grid-cols-12 gap-x-4 gap-y-2">
+                    <div class="col-span-12 lg:col-span-4 ">
+                        <label class="font-semibold text-white " for="tema">Tema: </label>
                         <input class="form-control " placeholder="Ingrese tema: " type="text" name="tema">
                     </div>
-                    <div class="tw-col-span-12 lg:tw-col-span-4">
-                        <label class="text-white font-semibold " for="fecha">Desde: </label>
-                        <input class="form-control " type="date" name="desde">
+                    <div class="col-span-12 lg:col-span-4">
+                        <label class="font-semibold text-white " for="fecha">Desde: </label>
+                        <input class="form-control" type="date" name="desde">
                     </div>
-                    <div class="tw-col-span-12 lg:tw-col-span-4">
-                        <label class="text-white font-semibold " for="fecha">Hasta: </label>
+                    <div class="col-span-12 lg:col-span-4">
+                        <label class="font-semibold text-white " for="fecha">Hasta: </label>
                         <input class="form-control " type="date" name="hasta">
 
                     </div>
                 </div>
-                <button type="submit" name="submit" class=" btn btn-primary tw-mt-4">Buscar</button>
+                <button type="submit" name="submit" class=" btn btn-primary mt-4">Buscar</button>
             </form>
             <div>
                 <?php
@@ -85,10 +121,10 @@ session_start();
                     if ($filas) {
                 ?>
 
-                        <p class="alert alert-success text-center w-50 m-auto">Se han encontrado resultados</p>
+                        <p class="m-auto text-center alert alert-success w-50">Se han encontrado resultados</p>
                         <?php
 
-                        echo "<h1 class='text-center mt-2 text-white'>Resultados de búsqueda</h1>";
+                        echo "<h1 class='mt-2 text-center text-white'>Resultados de búsqueda</h1>";
                         $contador2 = 0;
                         while ($acta = $resultado->fetch_row()) {
 
@@ -96,18 +132,18 @@ session_start();
                             if ($acta['0'] != 0) {
                         ?>
 
-                                <div class="tw-py-6">
-                                    <div class="tw-flex tw-flex-col tw-max-w-3xl tw-mx-auto tw-overflow-hidden tw-bg-white tw-rounded-lg tw-shadow-lg md:tw-flex-row">
-                                        <div class="tw-w-full tw-h-64 tw-bg-cover tw-md:tw-h-auto md:tw-w-1/3" style="background-image: url('https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80')">
+                                <div class="py-6">
+                                    <div class="flex flex-col max-w-3xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg md:flex-row">
+                                        <div class="w-full h-64 bg-cover md:h-auto md:w-1/3" style="background-image: url('https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80')">
                                         </div>
-                                        <div class="tw-w-2/3 tw-p-4 tw-flex tw-flex-col">
-                                            <h2 class="tw-text-2xl tw-font-bold tw-text-gray-900"><?php echo $acta['1']; ?></h2>
-                                            <p class="tw-text-gray-800"><span class="tw-text-gray-600">Descripcion: <?php echo $acta['5'] ?></span></p>
-                                            <div class="tw-flex tw-justify-between tw-mt-3 tw-item-center">
+                                        <div class="w-2/3 p-4 flex flex-col">
+                                            <h2 class="text-2xl font-bold text-gray-900"><?php echo $acta['1']; ?></h2>
+                                            <p class="text-gray-800"><span class="text-gray-600">Descripcion: <?php echo $acta['5'] ?></span></p>
+                                            <div class="flex justify-between mt-3 item-center">
                                                 <div>
-                                                    <h2 class="tw-inline-flex tw-items-center tw-text-gray-700">
+                                                    <h2 class="inline-flex items-center text-gray-700">
                                                         <span>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-6 tw-h-6 tw-mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                             </svg>
                                                         </span>
@@ -116,9 +152,9 @@ session_start();
                                                     </h2>
                                                 </div>
                                                 <div>
-                                                    <h2 class="tw-inline-flex tw-items-center tw-text-gray-700 ">
+                                                    <h2 class="inline-flex items-center text-gray-700 ">
                                                         <span>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-6 tw-h-6 tw-mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                             </svg>
                                                         </span>
@@ -129,11 +165,11 @@ session_start();
 
 
                                             </div>
-                                            <div class="tw-flex tw-justify-between tw-mt-3 tw-item-center">
+                                            <div class="flex justify-between mt-3 item-center">
                                                 <div>
-                                                    <a href="../actas/views/informacion-acta.php?codigo_acta=<?php echo $acta['0'] ?>" class="tw-inline-flex tw-mt-6 tw-font-semibold tw-text-blue-600 hover:tw-text-blue-400">
+                                                    <a href="../actas/views/informacion-acta.php?codigo_acta=<?php echo $acta['0'] ?>" class="inline-flex mt-6 font-semibold text-blue-600 hover:text-blue-400">
                                                         <span>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" class="tw-h-6 tw-w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                             </svg>
                                                         </span>
@@ -142,9 +178,9 @@ session_start();
                                                 <?php if ($_SESSION['rol']) { ?>
 
                                                     <div>
-                                                        <a href="../actas/views/editar-acta.php?codigo_acta=<?php echo $acta['0'] ?>" class="tw-inline-flex tw-mt-6 tw-font-semibold tw-text-blue-600 hover:tw-text-blue-400">
+                                                        <a href="../actas/views/editar-acta.php?codigo_acta=<?php echo $acta['0'] ?>" class="inline-flex mt-6 font-semibold text-blue-600 hover:text-blue-400">
                                                             <span>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" class="tw-h-6 tw-w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                                 </svg>
                                                             </span>
@@ -153,7 +189,7 @@ session_start();
 
                                                 <?php } else if ($_SESSION['rol'] != 'directiva') {  ?>
                                                     <div style="max-width:200px">
-                                                        <p class="tw-text-xs">No manejos los permisos suficientes para modificar el acta.</p>
+                                                        <p class="text-xs">No manejos los permisos suficientes para modificar el acta.</p>
                                                     </div>
                                                 <?php }
                                                 ?>
@@ -166,7 +202,7 @@ session_start();
                         }
                     } else {
                         ?>
-                        <p class="alert alert-danger text-center w-50 m-auto">No se encontraron resultados</p>
+                        <p class="m-auto text-center alert alert-danger w-50">No se encontraron resultados</p>
                 <?php
                     }
                     mysqli_free_result($resultado);
@@ -175,11 +211,11 @@ session_start();
 
                 ?>
             </div>
-            <div class="tw-fixed tw-bottom-0 tw-mb-6 tw-right-0 tw-mr-6">
+            <div class="fixed bottom-0 mb-6 right-0 mr-6">
 
                 <a href="../actas/views/crear-acta-cero.php">
-                    <button class="tw-text-white tw-px-4 tw-w-auto tw-h-16 tw-bg-green-500 tw-rounded-full hover:tw-bg-green-600 active:tw-shadow-lg tw-mouse tw-shadow tw-transition tw-ease-in tw-duration-200 focus:tw-outline-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-6 tw-h-6 tw-inline-block tw-mr-1"" fill=" none" viewBox="0 0 24 24" stroke="currentColor">
+                    <button class="text-white px-4 w-auto h-16 bg-green-500 rounded-full hover:bg-green-600 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 inline-block mr-1"" fill=" none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <span>Crear acta</span>
