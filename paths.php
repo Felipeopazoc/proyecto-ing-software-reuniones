0b71cecfab8ada2a370278ec236c1daeb7403773
vicentematus <vicentematusortiz@gmail.com>
2021-12-17 14:30:03 -0300

    Se agrego Templates del menu y paths.php para poder importar

---
+++ paths.php
@@ -0,0 +1,9 @@
+<?php
+
+
+defined("TEMPLATES_PATH")
+	or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/src/templates'));
+
+
+defined("TEMPLATES_PATH")
+	or define("DIRECTIVA_PATH", realpath(dirname(__FILE__) . '/src/templates'));
---
+++ src/templates/menu/header-tailwind.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		#menu-toggle:checked+#menu {
+			display: block;
+		}
+	</style>
+</head>
+
+<body>
+	<?php
+	if ($_SESSION['rol'] == "directiva") { ?>
+		<header style="background:#141A32" class="lg:px-16 px-6 flex flex-wrap lg:flex-row-reverse items-center lg:py-6 py-6">
+			<div class="flex-1 lg:flex-initial flex justify-between items-center">
+				<a class="bg-red-500 px-4 py-2 rounded-md text-white" href="../login/backend/cerrar.php"> Cerrar Sesión </a>
+			</div>
+
+			<label for="menu-toggle" class="pointer-cursor lg:hidden block">
+				<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+					<title>menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</label>
+			<input class="hidden" type="checkbox" id="menu-toggle" />
+
+			<div class="hidden lg:flex-1 lg:flex lg:items-center lg:w-auto w-full text-white" id="menu">
+				<nav>
+					<ul class="lg:flex items-center justify-between text-base pt-4 lg:pt-0">
+						<li>
+							<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="./index.php">Inicio</a>
+						</li>
+						<li>
+							<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="../reuniones/index.php">Agendar Reunion</a>
+						</li>
+						<li>
+							<a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Registrar Acta</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+
+
+	<?php } else {
+		echo "Happy";
+		echo $_SESSION['rol'];
+	}
+	?>
+</body>
+
+</html>
