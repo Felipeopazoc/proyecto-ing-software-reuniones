3b4d3b647f658c56e7dada2c54d6299bf609d8c4
vicentematus <vicentematusortiz@gmail.com>
2021-12-23 02:20:49 -0300

    cambios

--- actas/backend/recibir-acta.php
+++ actas/backend/recibir-acta.php
@@ -13,10 +13,19 @@ if (isset($_POST["submit"])) {
 
     $estado = $_POST["estado"];
     $codigo_acta = $_POST["codigo_acta"];
+    echo "
+        tema: $tema <br>
+        descripcion: $descripcion <br>
+        fecha: $reu_fecha <br>
+        hora_inicio: $hora_inicio <br>
+        hora_termino: $hora_termino <br>
+        estado: $estado <br>
+        codigo_acta: $codigo_acta <br>
+        ";
 
-    $query = "update acta set tema = '$tema' , fecha = '$reu_fecha' , hora_inicio = '$hora_inicio' , hora_termino = '$hora_termino' , estado = '$estado' , descripcion = '$descripcion' where codigo_acta = '$codigo_acta'";
-    mysqli_query($conn, $query);
-    header("location:./exito-acta.php?codigo_acta=$codigo_acta");
+    // $query = "update acta set tema = '$tema' , fecha = '$reu_fecha' , hora_inicio = '$hora_inicio' , hora_termino = '$hora_termino' , estado = '$estado' , descripcion = '$descripcion' where codigo_acta = '$codigo_acta'";
+    // mysqli_query($conn, $query);
+    // header("location:./exito-acta.php?codigo_acta=$codigo_acta");
     //$sql3 = $sql1 . $sql2;
     //echo $sql3;
     // $sql = "update acta set tema='$tema', fecha='$reu_fecha', hora_inicio ='$hora_inicio', hora_termino='$hora_termino', descripcion='$descrid_estado=$estado  where codigo_acta=$codigo_acta";
